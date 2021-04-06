914c173bb6fd266bac5869c66cc7dc4b2ccc130e
Lonny Gomes <code@lonnygomes.com>
2021-04-05 21:32:47 -0400

    chore: refactor code for clarity

--- pennydog-lamp.ino
+++ pennydog-lamp.ino
@@ -6,6 +6,8 @@ CRGB leds[NUM_LEDS];
 #define SOUND_BUF_LEN 5
 #define LOUDNESS_THRESHOLD 365
 #define LOUDNESS_TIMER_COUNT 100
+#define HUE_CLR_GREEN 96
+#define HUE_CLR_RED 255
 
 int sensorPin =A0 ;  // define analog port A0
 int value = 0;    //set value to 0
@@ -26,13 +28,16 @@ void setup() {
 
 void loop() 
 {
+  int hueMeanVal;
+
+  // retreive current audio sound level
   value = analogRead(sensorPin);  //set the value as the value read from A0
   if (value >= 300) {
     Serial.println(value, DEC);  //print the value and line wrap
   }
 
-  // retreive current audio sound level
-  int hueVal = map(value, 0, LOUDNESS_THRESHOLD, 96, 255);
+  // map audio value to hue color
+  int hueVal = map(value, 0, LOUDNESS_THRESHOLD, HUE_CLR_GREEN, HUE_CLR_RED);
   int hueSum = hueVal;
 
   // if we reach a loadness threshold, start the timer
@@ -40,6 +45,7 @@ void loop()
       loudnessTimer = LOUDNESS_TIMER_COUNT;
   }
 
+  // calc sum of buffer minus the oldest value
   for (int idx = 1; idx < SOUND_BUF_LEN; idx++) {
     hueSum += SOUND_BUF[idx];
     // shift values in sound buffer queue
@@ -49,15 +55,16 @@ void loop()
   // add hueVal to end of sound buffer queue
   SOUND_BUF[SOUND_BUF_LEN - 1] = hueVal;
 
-  // compute average sound level of buffer
-  int hueMeanVal = hueSum / SOUND_BUF_LEN;
-
   // decrement the counter
   if (loudnessTimer > 0) {
     loudnessTimer--;
     hueMeanVal = 0; // set to red
+  } else {
+    // compute average sound level of buffer
+    hueMeanVal = hueSum / SOUND_BUF_LEN;
   }
 
+  // update colors for each LED
   for(int dot = 0; dot < NUM_LEDS; dot++) {
     leds[dot].setHSV( hueMeanVal, 255, 255);
 
