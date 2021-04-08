7fdf1f13b74b5da076a3eb319a2bd3ffe4ba3488
Lonny Gomes <code@lonnygomes.com>
2021-04-08 00:07:12 -0400

    feat: add mode toggle switch
    toggles bettwen audio sync mode and lamp mode

--- pennydog-lamp.ino
+++ pennydog-lamp.ino
@@ -3,6 +3,7 @@
 #define NUM_LEDS 7
 CRGB leds[NUM_LEDS];
 #define NEOPIXEL_DATA_PIN 6
+#define MODE_TOGGLE_SWITCH_PIN 7
 #define SOUND_BUF_LEN 5
 #define LOUDNESS_THRESHOLD 365
 #define LOUDNESS_TIMER_COUNT 100
@@ -18,6 +19,9 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600); //set the baud rate to 9600
 
+  // set internal pull up resistor for toggle switch pin
+  pinMode(MODE_TOGGLE_SWITCH_PIN, INPUT_PULLUP);
+
   // zero out sound value buffer to start fresh
   for (int idx = 0; idx < SOUND_BUF_LEN; idx++) {
     SOUND_BUF[idx] = 0;
@@ -29,6 +33,10 @@ void setup() {
 void loop() 
 {
   int hueMeanVal;
+  int modeToggleState;
+
+  // retrieve current state of toggle
+  modeToggleState = digitalRead(MODE_TOGGLE_SWITCH_PIN);
 
   // retreive current audio sound level
   value = analogRead(sensorPin);  //set the value as the value read from A0
@@ -36,40 +44,48 @@ void loop()
     Serial.println(value, DEC);  //print the value and line wrap
   }
 
-  // map audio value to hue color
-  int hueVal = map(value, 0, LOUDNESS_THRESHOLD, HUE_CLR_GREEN, HUE_CLR_RED);
-  int hueSum = hueVal;
+  if (modeToggleState == HIGH) {
+    // map audio value to hue color
+    int hueVal = map(value, 0, LOUDNESS_THRESHOLD, HUE_CLR_GREEN, HUE_CLR_RED);
+    int hueSum = hueVal;
 
-  // if we reach a loadness threshold, start the timer
-  if (value >= LOUDNESS_THRESHOLD) {
-      loudnessTimer = LOUDNESS_TIMER_COUNT;
-  }
+    // if we reach a loadness threshold, start the timer
+    if (value >= LOUDNESS_THRESHOLD) {
+        loudnessTimer = LOUDNESS_TIMER_COUNT;
+    }
 
-  // calc sum of buffer minus the oldest value
-  for (int idx = 1; idx < SOUND_BUF_LEN; idx++) {
-    hueSum += SOUND_BUF[idx];
-    // shift values in sound buffer queue
-    SOUND_BUF[idx - 1] = SOUND_BUF[idx];
-  }
+    // calc sum of buffer minus the oldest value
+    for (int idx = 1; idx < SOUND_BUF_LEN; idx++) {
+      hueSum += SOUND_BUF[idx];
+      // shift values in sound buffer queue
+      SOUND_BUF[idx - 1] = SOUND_BUF[idx];
+    }
 
-  // add hueVal to end of sound buffer queue
-  SOUND_BUF[SOUND_BUF_LEN - 1] = hueVal;
+    // add hueVal to end of sound buffer queue
+    SOUND_BUF[SOUND_BUF_LEN - 1] = hueVal;
 
-  // decrement the counter
-  if (loudnessTimer > 0) {
-    loudnessTimer--;
-    hueMeanVal = 0; // set to red
+    // decrement the counter
+    if (loudnessTimer > 0) {
+      loudnessTimer--;
+      hueMeanVal = 0; // set to red
+    } else {
+      // compute average sound level of buffer
+      hueMeanVal = hueSum / SOUND_BUF_LEN;
+    }
+
+    // update colors for each LED
+    for(int dot = 0; dot < NUM_LEDS; dot++) {
+      leds[dot].setHSV( hueMeanVal, 255, 255);
+
+      // clear this led for the next time around the loop
+      //leds[dot] = CRGB::Black;
+    }
   } else {
-    // compute average sound level of buffer
-    hueMeanVal = hueSum / SOUND_BUF_LEN;
-  }
-
-  // update colors for each LED
-  for(int dot = 0; dot < NUM_LEDS; dot++) {
-    leds[dot].setHSV( hueMeanVal, 255, 255);
-
-    // clear this led for the next time around the loop
-    //leds[dot] = CRGB::Black;
+    int brightnessVal = 255;
+    for(int dot = 0; dot < NUM_LEDS; dot++) {
+      // clear this led for the next time around the loop
+      leds[dot].setHSV(0, 0, brightnessVal);
+    }
   }
 
   FastLED.show();
